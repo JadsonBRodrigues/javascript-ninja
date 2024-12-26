2bcd16e2234aa769b67f6122f35a48980db85e32
Jadson Rodrigues <jadsonbrodrigues@gmail.com>
2024-12-26 16:20:57 +0000

    Update challenge-07.js

--- curso javascript-ninja/challenge 7/challenge-07.js
+++ curso javascript-ninja/challenge 7/challenge-07.js
@@ -72,8 +72,9 @@ console.log( 'Números pares entre 10 e 20:' );
 20
 
 Na mesma ideia do exercício acima: mostre agora os números ímpares.
-*/
 console.log( 'Números ímpares entre 10 e 20:' );
+*/
+
 
  while (num >= 10 && num <20) {
 ... if (num++ % 2 === 0) console.log(num)}
@@ -89,13 +90,17 @@ Repita os mesmos exercícios feitos acima, mas agora usando o loop "for".
 Só vamos mudar o range:
 - No primeiro "for", mostre os números pares entre 100 e 120, inclusive eles;
 - No segundo "for", mostre os números ímpares entre 111 e 125, inclusive eles.
-*/
-console.log( 'Números pares entre 100 e 120:' );
 
-for (var num = 100; num >=100 && num <=120; num++) {
-... num % 2 === 0 ? console.log(num) : null;
+console.log( 'Números pares entre 100 e 120:' );
+*/
+
+for (var num = 100; num >= 100 && num <=120; num++) {
+... if (num % 2 === 0) console.log(num)
 ... }
 
+/*
 console.log( 'Números ímpares entre 111 e 125:' );
-// ?
+*/
 
+for (var num1 = 110; num1 >=110 && num1 <=125; num1) {
+...  if (num1++ % 2 === 0) console.log(num1) }
